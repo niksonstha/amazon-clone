6b8fca509e3b87fe8671d0ba9eefd561c4f60ec1
niksonstha <sthanikson10@gmail.com>
2023-11-03 21:52:32 +0545

    home banner done

---
+++ src/components/Product.jsx
@@ -0,0 +1,8 @@
+import "./Product.css";
+function Product() {
+  return <div className="product">
+    
+  </div>;
+}
+
+export default Product;
---
+++ src/screens/Home.jsx
@@ -0,0 +1,20 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://img.freepik.com/premium-photo/shopping-cart-symbol-with-torn-paper_220873-11807.jpg?w=1380"
+          alt=""
+        />
+        <div className="gradient-overlay"></div>
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
